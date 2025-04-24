c96cf411a94528df650fff54467eae18f0f8900e
Vidal Orlando Acosta Garcia <voacosta@xetid.cu>
2025-04-24 10:00:09 -0400

    Inicializando

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * Behaviors for the Tema personalizado BS5 theme.
+ */
+
+(function ($, Drupal) {
+  Drupal.behaviors.temaPersonalizadoBS5 = {
+    attach() {
+      // Tema personalizado BS5 JavaScript behaviors goes here.
+    },
+  };
+})(window.jQuery, window.Drupal);
